c0da9b72a9bb411b5fcf5d644aaf58859ad3079a
kristaps <>
2015-10-23 05:56:34 +0000

    Don't require captive players to click "login".

--- playerautoadd.js
+++ playerautoadd.js
@@ -1,5 +1,69 @@
 "use strict";
 
+function doLogErr(err) 
+{
+
+	if (409 == err) 
+		setTimeout(doRegTimeout, 10000);
+	else
+		doUnhide('loginSysError');
+}
+
+function doLogOk(resp) 
+{
+	var res;
+
+	try {
+		res = JSON.parse(resp);
+	} catch (error) {
+		location.href = '@HTURI@/playerhome.html';
+		return;
+	}
+	location.href = res.needjoin ?
+		'@HTURI@/playerlobby.html' :
+		'@HTURI@/playerhome.html';
+}
+
+function doLogSetup() 
+{
+
+	doHide('loginSysError');
+}
+
+function doRegTimeout()
+{
+
+	sendForm(document.getElementById('loginSection'),
+		doLogSetup, doLogErr, doLogOk);
+}
+
+function doRegOk(resp) 
+{
+	var res;
+
+	doHide('registerSection');
+	doUnhide('loginSection');
+
+	try {
+		res = JSON.parse(resp);
+	} catch (error) {
+		doUnhide('loginSysError');
+		return;
+	}
+
+	doValue('ident', res.ident);
+	doValue('password', res.password);
+	doRegTimeout();
+}
+
+function doRegSetup() 
+{
+
+	doHide('registerError');
+	doHide('registerSysError');
+	doValue('registering', 'Registering...');
+}
+
 function doRegErr(err) 
 {
 
@@ -21,68 +85,4 @@ function doRegErr(err)
 		doUnhide('registerSysError');
 		break;
 	}
-}
-
-function doLogErr(err) 
-{
-
-	doValue('loggingin', 'Login');
-	doUnhide(409 == err ? 'loginError' : 'loginSysError');
-}
-
-/*
- * Handle success from the dologin.json script.
- * This will return a small JSON body that indicates whether we should
- * redirect to the main page for play or into the lobby.
- * In the case of errors (uh-oh), just try the main page.
- */
-function doLogOk(resp) 
-{
-	var res;
-
-	doValue('loggingin', 'Success! Redirecting...');
-
-	try {
-		res = JSON.parse(resp);
-	} catch (error) {
-		location.href = '@HTURI@/playerhome.html';
-		return;
-	}
-	location.href = res.needjoin ?
-		'@HTURI@/playerlobby.html' :
-		'@HTURI@/playerhome.html';
-}
-
-function doRegOk(resp) 
-{
-	var res;
-
-	doHide('registerSection');
-	doUnhide('loginSection');
-
-	try {
-		res = JSON.parse(resp);
-	} catch (error) {
-		doUnhide('loginSysError');
-		return;
-	}
-
-	doValue('ident', res.ident);
-	doValue('password', res.password);
-}
-
-function doLogSetup() 
-{
-
-	doHide('loginError');
-	doHide('loginSysError');
-	doValue('registering', 'Logging in...');
-}
-
-function doRegSetup() 
-{
-
-	doHide('registerError');
-	doHide('registerSysError');
-	doValue('registering', 'Registering...');
 }
